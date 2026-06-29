1adb11fe0fdf8a720246ffadf41eaa0c333ca048
Felipe <fekabemarques@gmail.com>
2026-06-28 23:37:36 -0300

    Mais alguns testes e correções

--- School Managing System/School_Managing_System.py
+++ School Managing System/School_Managing_System.py
@@ -23,6 +23,10 @@ class Pessoa:
     def setId(self, id):
         self._id = id
 
+    def printAttributes(self):
+        print(f"Nome: {self._nome}\n"
+              f"Idade: {self._idade}\n"
+              f"Id: {self._id}.")
 
 class Aluno(Pessoa):
     def __init__(self, nome: str, idade: int, id: int):
@@ -36,8 +40,15 @@ class Aluno(Pessoa):
         return self._notas
 
     def getMedia(self) -> float:
-        media = sum(self._notas) / len(self._notas)
-        return media
+        if not self._notas:
+            return 0
+        return sum(self._notas) / len(self._notas)
+
+    def printAttributes(self):
+        super().printAttributes()
+        for i, nota in enumerate(self._notas, start=1):
+            print(f"Nota {i}: {nota}")
+        print(f"Media do aluno: {self.getMedia()}")
 
 class Professor(Pessoa):
     def __init__(self, nome: str, idade: int, id: int,
@@ -63,16 +74,44 @@ class Professor(Pessoa):
             print(f"{al.getNome()} não possui notas.")
             return
 
-        media = sum(al.getNotas()) / len(al.getNotas())
-
-        if media >= 5:
-            print(f"O aluno {al.getNome()} foi aprovado!")
+        if al.getMedia() >= 5:
+            print(f"O(a) professor(a) {Professor.getNome()} aprovou o aluno {al.getNome()}!")
         else:
-            print(f"O aluno {al.getNome()} foi reprovado!")
+            print(f"O(a) professor(a) {Professor.getNome()} reprovou o aluno {al.getNome()}!")
+
+    def printAttributes(self):
+        super().printAttributes()
+        print(f"Salário: {self._salario}\n"
+              f"Graduação: {self._graduacao}\n")
+
 
 aluno1 = Aluno("Felipe", 17, 1234)
 aluno1.setNotas(7)
 aluno1.setNotas(6)
 aluno1.setNotas(5)
 aluno1.setNotas(9)
-print(aluno1.getMedia())
+aluno1.printAttributes()
+
+print()
+
+aluno2 = Aluno("Laura", 16, 1235)
+aluno2.setNotas(8)
+aluno2.setNotas(8)
+aluno2.setNotas(9)
+aluno2.setNotas(10)
+aluno2.printAttributes()
+
+print()
+
+professor1 = Professor("Carlos", 67, 25, 4_569.34, 'Mestrado Em Letras e em Filosofia')
+professor1.printAttributes()
+
+print()
+
+print(f'Média Felipe: {aluno1.getMedia()}')
+professor1.aprovar_ou_reprovar(aluno1)
+
+print()
+
+print(f'Média Laura: {aluno2.getMedia()}')
+professor1.aprovar_ou_reprovar(aluno2)
